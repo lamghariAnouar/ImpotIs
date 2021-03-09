eea271ac97af57a14bba4e01a4d117d9811c58ea
hajkati <79836425+hajkati@users.noreply.github.com>
2021-03-09 14:53:04 +0100

    Commit

--- src/main/java/com/example/demo/service/FactureService.java
+++ src/main/java/com/example/demo/service/FactureService.java
@@ -2,21 +2,41 @@ package com.example.demo.service;
 
 import java.util.List;
 
+import javax.persistence.EntityManager;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.bean.Facture;
 import com.example.demo.dao.FactureDao;
+import com.example.demo.vo.FactureVo;
 @Service
 public class FactureService {
 	@Autowired
 	private FactureDao factureDao;
+	
 
 	
 
 	//@Autowired
 	//SocieteService societeservice;
 
+	public List<Facture> findByCriteria(FactureVo factureVo){
+		String query="SELECT f FROM Facture f WHERE 1=1 ";
+		if(factureVo.getMontantHtMin()!=null && !factureVo.getMontantHtMin().isEmpty())
+			query+=" AND f.montantHt >='"+factureVo.getMontantHtMin()+"'";
+		if(factureVo.getMontantHtMax()!=null && !factureVo.getMontantHtMax().isEmpty())
+			query+=" AND f.montantHt <='"+factureVo.getMontantHtMax()+"'";
+		if(factureVo.getMontantTtcMin()!=null && !factureVo.getMontantTtcMin().isEmpty())
+			query+=" AND f.montantTtc <='"+factureVo.getMontantTtcMin()+"'";
+		if(factureVo.getMontantTtcMax()!=null && !factureVo.getMontantTtcMax().isEmpty())
+			query+=" AND f.montantTtc >='"+factureVo.getMontantTtcMax()+"'";
+		if(factureVo.getMontantTvaMin()!=null && !factureVo.getMontantTvaMin().isEmpty())
+			query+=" AND f.montantTva >='"+factureVo.getMontantTvaMin()+"'";
+		if(factureVo.getMontantTvaMax()!=null && !factureVo.getMontantTvaMax().isEmpty())
+			query+=" AND f.montantTva <='"+factureVo.getMontantTvaMax()+"'";
+		return entityManager.createQuery(query).getResultList();
+	}
 	public Facture findByIcesocieteSource(String icesocieteSource) {
 		return factureDao.findByIcesocieteSource(icesocieteSource);
 	}
@@ -26,12 +46,10 @@ public class FactureService {
 	}
 
 	public int save(Facture facture) {
-		if(findByIcesocieteDestination(facture.getIcesocieteDestination())!=null) {
+		if(findByIcesocieteDestination(facture.getIcesocieteDestination())!=null || findByIcesocieteSource(facture.getIcesocieteSource())!= null) {
 		return -1;
 		}
-		if(findByIcesocieteSource(facture.getIcesocieteSource())!= null) {
-			return -2;
-			}
+		
 			else {
 			factureDao.save(facture);
 			return 1;
@@ -45,4 +63,6 @@ public class FactureService {
 	public void delete(Facture entity) {
 		factureDao.delete(entity);
 	}
+	@Autowired
+	private EntityManager entityManager;
 }
---
+++ src/main/java/com/example/demo/vo/FactureVo.java
@@ -0,0 +1,70 @@
+package com.example.demo.vo;
+
+public class FactureVo {
+
+	private String montantHt;
+	private String montantHtMin;
+	private String montantHtMax;
+	private String montantTtc;
+	private String montantTva;
+	private String montantTtcMin;
+	private String montantTvaMin;
+	private String montantTtcMax;
+	private String montantTvaMax;
+	public String getMontantHt() {
+		return montantHt;
+	}
+	public String getMontantHtMin() {
+		return montantHtMin;
+	}
+	public String getMontantHtMax() {
+		return montantHtMax;
+	}
+	
+	public void setMontantHt(String montantHt) {
+		this.montantHt = montantHt;
+	}
+	public void setMontantHtMin(String montantHtMin) {
+		this.montantHtMin = montantHtMin;
+	}
+	public void setMontantHtMax(String montantHtMax) {
+		this.montantHtMax = montantHtMax;
+	}
+	public String getMontantTtc() {
+		return montantTtc;
+	}
+	public String getMontantTtcMin() {
+		return montantTtcMin;
+	}
+	public String getMontantTtcMax() {
+		return montantTtcMax;
+	}
+	public void setMontantTtc(String montantTtc) {
+		this.montantTtc = montantTtc;
+	}
+	public void setMontantTtcMin(String montantTtcMin) {
+		this.montantTtcMin = montantTtcMin;
+	}
+	public void setMontantTtcMax(String montantTtcMax) {
+		this.montantTtcMax = montantTtcMax;
+	}
+	public String getMontantTva() {
+		return montantTva;
+	}
+	public String getMontantTvaMin() {
+		return montantTvaMin;
+	}
+	public String getMontantTvaMax() {
+		return montantTvaMax;
+	}
+	public void setMontantTva(String montantTva) {
+		this.montantTva = montantTva;
+		
+	}
+	public void setMontantTvaMin(String montantTvaMin) {
+		this.montantTvaMin = montantTvaMin;
+	}
+	public void setMontantTvaMax(String montantTvaMax) {
+		this.montantTvaMax = montantTvaMax;
+	}
+}
